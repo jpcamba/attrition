ec41484367159a9b9427400efd099abc85cecf1a
roannadavid <roannadavid@yahoo.com>
2016-05-27 12:30:32 +0800

    Renamed migrations

--- app/database/migrations/2016_03_28_094613_recreate_correlations_table.php
+++ app/database/migrations/2016_03_28_094613_recreate_correlations_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCorrelationsTable extends Migration {
+class RereateCorrelationsTable extends Migration {
 
 	/**
 	 * Run the migrations.
