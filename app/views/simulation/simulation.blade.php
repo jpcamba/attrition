06098b8d8e2e8ef7c6d34a2b8797078e2a5f126a
roannadavid <roannadavid@yahoo.com>
2016-06-01 16:15:32 +0800

    Added Units button in Simulation Page

--- app/views/simulation/simulation.blade.php
+++ app/views/simulation/simulation.blade.php
@@ -28,15 +28,19 @@
                 <h4>Choose individual factor for simulation of attrition rate.</h4> <br>
                 <center>
                     <div class="col-md-12">
-                        <a href="/simulation/employment" class="btn btn-primary btn-lg col-md-5">Employment</a>
-                        <div class="col-md-2"></div>
-                        <a href="/simulation/grades" class="btn btn-danger btn-lg col-md-5">Grades</a>
+                    	<a href="/simulation/employment" class="btn btn-primary btn-lg col-md-5">Employment</a>
+                    	<div class="col-md-2"></div>
+                    	<a href="/simulation/grades" class="btn btn-danger btn-lg col-md-5">Grades</a>
                     </div>
                     <div class="col-md-12" style="height:20px"></div>
                     <div class="col-md-12">
-                        <a href="/simulation/region" class="btn btn-success btn-lg col-md-5">Region</a>
-                        <div class="col-md-2"></div>
-                        <a href="/simulation/stbracket" class="btn btn-info btn-lg col-md-5">ST Bracket</a>
+                    	<a href="/simulation/region" class="btn btn-success btn-lg col-md-5">Region</a>
+                    	<div class="col-md-2"></div>
+                    	<a href="/simulation/stbracket" class="btn btn-info btn-lg col-md-5">ST Bracket</a>
+                    </div>
+                    <div class="col-md-12" style="height:20px"></div>
+                    <div class="col-md-12">
+                    	<a href="/simulation/units" class="btn btn-warning btn-lg col-md-5">Units</a>
                     </div>
                 </center>
             </div>
